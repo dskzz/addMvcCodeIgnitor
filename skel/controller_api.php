f1884d0e3242f015cabfd4845008f9a3b2097b85
dskzz <dnmiller1@gmail.com>
2022-04-24 15:06:23 -0400

    Update controller_api.php
    forgot to save the fixed controllernamehere

--- skel/controller_api.php
+++ skel/controller_api.php
@@ -9,25 +9,21 @@ class CONTROLLERNAMEHERE extends MY_Controller {
 	public function __construct()
 	{
 			parent::__construct();
-
-			$this->data['title'] = 'Listography';
 			$this->data['name']  = 'CONTROLLERNAMEHERE';
 			$this->data['SITE_URL'] = SITE_URL;
 			$this->data['ACTIVE_CONTROLLER'] = 'CONTROLLERNAMEHERE';
-			$this->data['js_array']  = array( QUOTESCRIPTNAMEHERE );
 			$this->load->model( 'MODELNAMEHERE' );
 			// Your own constructor code
 	}
 
-	private function _finish(  )
+	private function _finish( $result )
 	{
-		$this->smarty->view('index.tpl', $this->data);
+		return json_encode( $result );
 	}
 
 	public function index()
 	{
 		$this->MODELNAMEHERE->getMODELNAMEHERE();
-		$this->data['viewPage'] = strtolower( 'panels/LOWERNAMEHERE/LOWERNAMEHERE.tpl' )
 		$this->_finish(  );
 	}
 
